de80ffc0f221c9398ad9fa92ee0d6c7fa7d797ed
jazib-zaidi <jazib@feedops.com>
2025-03-24 12:34:13 +0530

    Initial commit - Fetch utility & LoadingSpinner

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-export * from './fetchData.js';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export * from resolve(__dirname, './fetchData.js');
