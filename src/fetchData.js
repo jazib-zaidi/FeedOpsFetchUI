9785360b9acea255066cdd25d320d89307d7c0cc
jazib-zaidi <jazib@feedops.com>
2025-03-24 12:27:47 +0530

    Initial commit - Fetch utility & LoadingSpinner

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,12 +1,4 @@
 export const fetchData = async (url) => {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status}`);
-    }
-    return await response.json();
-  } catch (error) {
-    console.error('Fetch error:', error);
-    return null;
-  }
+  console.log(url);
+  return url;
 };
